93b4a91ef810fca54006d33d129c4265f91f2721
Philipp Gabler <phips.gabler@gmail.com>
2016-06-24 13:41:09 +0200

    Clean up dependencies; move stuff into class.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
-class Future {
 
+const fl = require('fantasy-land');
+
+class Future {
   constructor(action) {
     this.action = action;
   }
@@ -9,45 +11,45 @@ class Future {
     this.action(err, succ);
   }
 
-  map(fn) {
+  [fl.map](fn) {
     return this.chain(x => Future.of(fn(x)));
   }
 
-  chain(fn) {
+  [fl.chain](fn) {
     return new Future((reject, resolve) => 
       this.fork(e => reject(e),
                 data => fn(data).fork(reject, resolve)));
   }
-};
 
-Future.of = function(x) {
-  return new Future((_, resolve) => resolve(x));
-};
+  static [fl.of](x) {
+    return new Future((_, resolve) => resolve(x));
+  }
 
-Future.all = function(futs) {
-  return new Future((reject, resolve) => {
-    let results = [];
-    let count = 0;
-    let done = false;
+  static all(futs) {
+    return new Future((reject, resolve) => {
+      let results = [];
+      let count = 0;
+      let done = false;
 
-    futs.forEach((fut, i) => {
-      fut.fork( 
-        error => { 
-          if(!done) {
-            done = true;
-            reject(error)
-          }
-        },
-        result => {
-          results[i] = result;
-          count += 1;
-          if (count === futs.length) {
-            resolve(results);
-          }
-        })
+      futs.forEach((fut, i) => {
+        fut.fork(
+            error => {
+              if(!done) {
+                done = true;
+                reject(error)
+              }
+            },
+            result => {
+              results[i] = result;
+              count += 1;
+              if (count === futs.length) {
+                resolve(results);
+              }
+            })
+      });
+
     });
-         
-  });
+  }
 }
 
 module.exports = {
